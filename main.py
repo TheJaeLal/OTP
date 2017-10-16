fcf0a9b252d9f67d1d43d93b3ca51659fc850069
Jay Lal <jay.lal@somaiya.edu>
2017-10-16 23:50:34 +0530

    Removed message.py dependancy

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-import message
+import sys
 import otp
 import sms
 
+#Lenght of OTP (digits)
 otp_size = 5
-target = message.to
+
+#The target mobile number, passed as a commandline argument...
+target = sys.argv[1]
 
 def main():
+	#Get new otp
 	new_otp = otp.generate(otp_size)
 	
+	#Craft the message to be sent
 	msg = "Your {0} digit OTP is as follows: {1}, it expires in 2 minutes!".format(otp_size,new_otp)
 	
+	#Send the message
 	sms.send(target,msg)
 
 if __name__=='__main__':
