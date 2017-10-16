9377ae5c204af520914b5a206d2e7bdc511a3343
Jay Lal <jay.lal@somaiya.edu>
2017-10-16 23:41:42 +0530

    fixed otp=otp.generate to new_otp=otp.generate

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ otp_size = 5
 target = message.to
 
 def main():
-	otp = otp.generate(otp_size)
+	new_otp = otp.generate(otp_size)
 	
-	msg = "Your {0} digit OTP is as follows: {1}, it expires in 2 minutes!".format(otp_size,otp)
+	msg = "Your {0} digit OTP is as follows: {1}, it expires in 2 minutes!".format(otp_size,new_otp)
 	
 	sms.send(target,msg)
 
